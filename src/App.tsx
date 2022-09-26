065636bdbf78ac8047b61c7ed4cfd5e607bf8153
AngelikaPrass <75100203+AngelikaPrass@users.noreply.github.com>
2022-09-26 23:21:44 +0200

    countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+import Countdown from "./components/countdown/countdown";
+
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Countdown />
+        <header className="App-header">
+        </header>
     </div>
   );
 }
